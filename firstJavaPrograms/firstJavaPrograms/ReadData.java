9f64a70a25005ba36437501a217b65ba0679a6ee
PremFTW <premtech16@gmail.com>
2022-01-20 11:10:41 -0500

    Deleted the empty project folder

---
+++ firstJavaPrograms/firstJavaPrograms/ReadData.java
@@ -0,0 +1,26 @@
+public class ReadData {
+  public static void main(String[] args) throws Exception {
+    // Create a File instance
+    java.io.File file = new java.io.File("scores.txt");
+	if (!file.exists()) {
+	      System.out.println("File does not exist");
+	      System.exit(0);
+	}
+
+    // Create a Scanner for the file
+    java.util.Scanner input = new java.util.Scanner(file);
+
+    // Read data from a file
+    while (input.hasNext()) {
+      String firstName = input.next();
+      String mi = input.next();
+      String lastName = input.next();
+      int score = input.nextInt();
+      System.out.println(
+        firstName + " " + mi + " " + lastName + " " + score);
+    }
+
+    // Close the file
+    input.close();
+  }
+}
